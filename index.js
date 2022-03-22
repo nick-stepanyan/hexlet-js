c8426e6e65e7afb62a587e9823dc4e26a9fce3ad
nick <nick.win.32@mail.ru>
2022-03-22 17:37:43 +0300

    some corrector file

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-import _ from "lodash";
-console.log(_.last([one, two]));
+{
+    "name": "hexlet-js-repository",
+    "version": "1.0.0",
+    "description": "Мой первый проект на Хекслете Ура, получилось синхронизировать и запушить!",
+    "main": "index.js",
+    "scripts": {
+        "test": "echo \"Error: no test specified\" && exit 1"
+    },
+    "repository": {
+        "type": "module",
+        "url": "git+https://github.com/nick-stepanyan/hexlet-js.git"
+    },
+    "author": "Nick",
+    "license": "ISC",
+    "bugs": {
+        "url": "https://github.com/nick-stepanyan/hexlet-js/issues"
+    },
+    "homepage": "https://github.com/nick-stepanyan/hexlet-js#readme",
+    "dependencies": {
+        "lodash": "^4.17.21"
+    }
+}
